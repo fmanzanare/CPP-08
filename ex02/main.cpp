3966d14f90c0208858e5ca9c34f0b0a7d774a723
Fernando Manzanares Montes <fmanzana@student.42malaga.com>
2023-08-02 19:12:02 +0200

    -ex00- executable deleted | -ex02- more tests added to main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmanzana <fmanzana@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 12:27:32 by fmanzana          #+#    #+#             */
-/*   Updated: 2023/07/31 12:44:00 by fmanzana         ###   ########.fr       */
+/*   Updated: 2023/08/02 19:10:54 by fmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 int main(void) {
 	MutantStack<int> mstack;
+
+	std::cout << "First operations over mstack: " << std::endl;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Top: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "Second operations over mstack: " << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -34,6 +40,13 @@ int main(void) {
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::cout << std::endl;
+
+	std::cout << "std::stack s build through copy constructor: " << std::endl;
 	std::stack<int> s(mstack);
-	return 0;
+	std::cout << "Top of std::stack s: " << s.top() << std::endl;
+	s.pop();
+	std::cout << "Top of std::stack s -after removing previus top element-: " << s.top() << std::endl;
+
+	return (0);
 }
